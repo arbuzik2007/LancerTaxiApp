c06c05f105059938c745ef9b0c0f85df530c8b72
arbuzik2007 <alenakoroleva2007@mail.ru>
2023-09-28 19:41:31 +0700

    Current stable version

--- TechODayApp/TechODayApp/Views/MapViewingPage.xaml.cs
+++ TechODayApp/TechODayApp/Views/MapViewingPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using TechODayApp.Services;
 using TechODayApp.ViewModels;
-using Xamarin.CommunityToolkit.Extensions;
-using Xamarin.CommunityToolkit.UI.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -17,44 +19,50 @@ namespace TechODayApp.Views
         {
             InitializeComponent();
             BindingContext = _viewModel = new DriversViewModel();
+            Load();
         }
-        protected override void OnAppearing()
+        private void Load()
         {
-            base.OnAppearing();
-            topLayout.IsVisible = true;
-            usersDisplay.IsVisible = true;
-            if (UserProfileState.Instance.IsDriverProfileVisible)
-            {
-                topLayout.IsVisible = false;
-                usersDisplay.IsVisible = false;
-                var request = DataService.Instance.DriveRequest;
-                // Change UI elements for a driver
-                var a = DataService.Instance.GetLastItemSimple();
-                if (request.SelectedDriver == a)
-                {
-                    var popup = new Popup
-                    {
-                        Content = new StackLayout
-                        {
-                            Children = {
-                            new Label { Text = request.SelectedDriver.DriverName},
-                            new Button{Text = "Accept", Command = new Command(() => Dismiss("Dismiss was clicked")) },
-                            new Button{Text = "Reject", Command = new Command(() => Dismiss("Dismiss was clicked")) }
-                        }
-                        }
-                    };
-                }
-            }
-            else
-            {
-                usersDisplay.IsVisible = true;
-            }
-            _viewModel.OnAppearing();
-        }
 
-        private void Dismiss(string v)
-        {
-            throw new NotImplementedException("Here the main coder went to sleep");
+
         }
+        //protected override void OnAppearing()
+        //{
+        //    base.OnAppearing();
+        //    topLayout.IsVisible = true;
+        //    usersDisplay.IsVisible = true;
+        //    if (UserProfileState.Instance.IsDriverProfileVisible)
+        //    {
+        //        topLayout.IsVisible = false;
+        //        usersDisplay.IsVisible = false;
+        //        var request = DataService.Instance.DriveRequest;
+        //        // Change UI elements for a driver
+        //        var a = DataService.Instance.GetLastItemSimple();
+        //        if (request.SelectedDriver == a)
+        //        {
+        //            var popup = new Popup
+        //            {
+        //                Content = new StackLayout
+        //                {
+        //                    Children = {
+        //                    new Label { Text = request.SelectedDriver.DriverName},
+        //                    new Button{Text = "Accept", Command = new Command(() => Dismiss("Dismiss was clicked")) },
+        //                    new Button{Text = "Reject", Command = new Command(() => Dismiss("Dismiss was clicked")) }
+        //                }
+        //                }
+        //            };
+        //        }
+        //    }
+        //    else
+        //    {
+        //        usersDisplay.IsVisible = true;
+        //    }
+        //    _viewModel.OnAppearing();
+        //}
+
+        //private void Dismiss(string v)
+        //{
+        //    throw new NotImplementedException("Here the main coder went to sleep");
+        //}
     }
 }
